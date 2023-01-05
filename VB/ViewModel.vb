784bfec5c4445d256506c066aeb21b562b3b7ad5
DevExpressExampleBot <fakeEmail>
2023-01-05 12:42:56 +0400

    Source auto update [skip ci]

--- VB/ViewModel.vb
+++ VB/ViewModel.vb
@@ -1,136 +1,101 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections
 Imports System.Collections.Generic
 Imports System.ComponentModel
 
 Namespace PLinqDataSource
-	Public Class PLinqViewModel
-		Implements INotifyPropertyChanged
-		Private orderDataListSource As OrderDataListSource
 
-		Public Sub New()
-			ListSource = New OrderDataListSource(1000000)
-		End Sub
+    Public Class PLinqViewModel
+        Implements INotifyPropertyChanged
 
-		Public Property ListSource() As OrderDataListSource
-			Get
-				Return orderDataListSource
-			End Get
-			Set(ByVal value As OrderDataListSource)
-				If orderDataListSource Is value Then
-					Return
-				End If
-				orderDataListSource = value
-				OnPropertyChanged("ListSource")
-			End Set
-		End Property
+        Private orderDataListSource As OrderDataListSource
 
-		Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+        Public Sub New()
+            ListSource = New OrderDataListSource(1000000)
+        End Sub
 
-		Protected Overridable Sub OnPropertyChanged(ByVal propertyName As String)
-			RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
-		End Sub
-	End Class
+        Public Property ListSource As OrderDataListSource
+            Get
+                Return orderDataListSource
+            End Get
 
-	Public Class OrderDataListSource
-		Implements IListSource
-		Private orders As List(Of OrderData)
-		Private count As Integer = 0
-		Private Shared Locker As Object = New Object()
+            Set(ByVal value As OrderDataListSource)
+                If orderDataListSource Is value Then Return
+                orderDataListSource = value
+                OnPropertyChanged("ListSource")
+            End Set
+        End Property
 
-		Public Sub New(ByVal count As Integer)
-			Me.count = count
-		End Sub
+        Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
 
-		Private Sub GenerateOrders()
-			orders = New List(Of OrderData)(count)
-			Dim rnd As New Random()
-			Dim customersCount As Integer = Data.Customers.Count
-			Dim productsCount As Integer = Data.Products.Count
-			For i As Integer = 0 To count - 1
-				Dim orderData As New OrderData()
-				orderData.OrderId = i + 1
-				orderData.CustomerName = Data.Customers(rnd.Next(customersCount))
-				orderData.OrderDate = DateTime.Today.Subtract(TimeSpan.FromDays(rnd.Next(180)))
-				Dim product As KeyValuePair(Of String, Decimal) = Data.Products(rnd.Next(productsCount))
-				orderData.ProductName = product.Key
-				orderData.Price = product.Value
-				orderData.Quantity = rnd.Next(200) + 1
-				orders.Add(orderData)
-			Next i
-		End Sub
-		Public Function GetList() As IList Implements IListSource.GetList
-			If orders Is Nothing Then
-				SyncLock Locker
-					If orders Is Nothing Then
-						GenerateOrders()
-					End If
-				End SyncLock
-			End If
-			Return orders
-		End Function
-		Public ReadOnly Property ContainsListCollection() As Boolean Implements IListSource.ContainsListCollection
-			Get
-				Return False
-			End Get
-		End Property
-	End Class
+        Protected Overridable Sub OnPropertyChanged(ByVal propertyName As String)
+            RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
+        End Sub
+    End Class
 
-	Public Class OrderData
-		Private privateOrderId As Integer
-		Public Property OrderId() As Integer
-			Get
-				Return privateOrderId
-			End Get
-			Set(ByVal value As Integer)
-				privateOrderId = value
-			End Set
-		End Property
-		Private privateCustomerName As String
-		Public Property CustomerName() As String
-			Get
-				Return privateCustomerName
-			End Get
-			Set(ByVal value As String)
-				privateCustomerName = value
-			End Set
-		End Property
-		Private privateOrderDate As DateTime
-		Public Property OrderDate() As DateTime
-			Get
-				Return privateOrderDate
-			End Get
-			Set(ByVal value As DateTime)
-				privateOrderDate = value
-			End Set
-		End Property
-		Private privateProductName As String
-		Public Property ProductName() As String
-			Get
-				Return privateProductName
-			End Get
-			Set(ByVal value As String)
-				privateProductName = value
-			End Set
-		End Property
-		Private privatePrice As Decimal
-		Public Property Price() As Decimal
-			Get
-				Return privatePrice
-			End Get
-			Set(ByVal value As Decimal)
-				privatePrice = value
-			End Set
-		End Property
-		Private privateQuantity As Integer
-		Public Property Quantity() As Integer
-			Get
-				Return privateQuantity
-			End Get
-			Set(ByVal value As Integer)
-				privateQuantity = value
-			End Set
-		End Property
-	End Class
+    Public Class OrderDataListSource
+        Implements IListSource
+
+        Private orders As List(Of OrderData)
+
+        Private count As Integer = 0
+
+        Private Shared Locker As Object = New Object()
+
+        Public Sub New(ByVal count As Integer)
+            Me.count = count
+        End Sub
+
+        Private Sub GenerateOrders()
+            orders = New List(Of OrderData)(count)
+            Dim rnd As Random = New Random()
+            Dim customersCount As Integer = Customers.Count
+            Dim productsCount As Integer = Products.Count
+            For i As Integer = 0 To count - 1
+                Dim orderData As OrderData = New OrderData()
+                orderData.OrderId = i + 1
+                orderData.CustomerName = Customers(rnd.Next(customersCount))
+                orderData.OrderDate = Date.Today.Subtract(TimeSpan.FromDays(rnd.Next(180)))
+                Dim product As KeyValuePair(Of String, Decimal) = Products(rnd.Next(productsCount))
+                orderData.ProductName = product.Key
+                orderData.Price = product.Value
+                orderData.Quantity = rnd.Next(200) + 1
+                orders.Add(orderData)
+            Next
+        End Sub
+
+        Public Function GetList() As IList Implements IListSource.GetList
+            If orders Is Nothing Then
+                SyncLock Locker
+                    If orders Is Nothing Then
+                        GenerateOrders()
+                    End If
+
+                End SyncLock
+            End If
+
+            Return orders
+        End Function
+
+        Public ReadOnly Property ContainsListCollection As Boolean Implements IListSource.ContainsListCollection
+            Get
+                Return False
+            End Get
+        End Property
+    End Class
+
+    Public Class OrderData
+
+        Public Property OrderId As Integer
+
+        Public Property CustomerName As String
+
+        Public Property OrderDate As Date
+
+        Public Property ProductName As String
+
+        Public Property Price As Decimal
+
+        Public Property Quantity As Integer
+    End Class
 End Namespace
