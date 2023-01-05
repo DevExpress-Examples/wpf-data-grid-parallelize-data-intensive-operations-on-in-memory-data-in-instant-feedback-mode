ce2a672ca5f7aa54b6934d5cab6f1b527c4c4f64
DevExpressExampleBot <fakeEmail>
2023-01-05 12:43:25 +0400

    Source auto update [skip ci]

--- VB/ViewModel.vb
+++ VB/ViewModel.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to parallelize data-intensive operations on in-memory data in Instant Feedback UI mode
 ' 
 ' In this demo, the DevExpress Grid Control for WPF operates in Instant Feedback
@@ -16,28 +16,30 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E3382
-
 Imports System
 Imports System.Collections
 Imports System.Collections.Generic
 Imports System.ComponentModel
 
 Namespace PLinqDataSource
+
     Public Class PLinqViewModel
 
         Public Sub New()
             ListSource = New OrderDataListSource(1000000)
         End Sub
 
-        Public Overridable Property ListSource() As OrderDataListSource
+        Public Overridable Property ListSource As OrderDataListSource
     End Class
 
     Public Class OrderDataListSource
         Implements IListSource
 
         Private orders As List(Of OrderData)
+
         Private count As Integer = 0
-        Private Shared Locker As New Object()
+
+        Private Shared Locker As Object = New Object()
 
         Public Sub New(ByVal count As Integer)
             Me.count = count
@@ -45,32 +47,36 @@ Namespace PLinqDataSource
 
         Private Sub GenerateOrders()
             orders = New List(Of OrderData)(count)
-            Dim rnd As New Random()
-            Dim customersCount As Integer = Data.Customers.Count
-            Dim productsCount As Integer = Data.Products.Count
+            Dim rnd As Random = New Random()
+            Dim customersCount As Integer = Customers.Count
+            Dim productsCount As Integer = Products.Count
             For i As Integer = 0 To count - 1
-                Dim orderData As New OrderData()
+                Dim orderData As OrderData = New OrderData()
                 orderData.OrderId = i + 1
-                orderData.CustomerName = Data.Customers(rnd.Next(customersCount))
+                orderData.CustomerName = Customers(rnd.Next(customersCount))
                 orderData.OrderDate = Date.Today.Subtract(TimeSpan.FromDays(rnd.Next(180)))
-                Dim product As KeyValuePair(Of String, Decimal) = Data.Products(rnd.Next(productsCount))
+                Dim product As KeyValuePair(Of String, Decimal) = Products(rnd.Next(productsCount))
                 orderData.ProductName = product.Key
                 orderData.Price = product.Value
                 orderData.Quantity = rnd.Next(200) + 1
                 orders.Add(orderData)
-            Next i
+            Next
         End Sub
+
         Public Function GetList() As IList Implements IListSource.GetList
             If orders Is Nothing Then
                 SyncLock Locker
                     If orders Is Nothing Then
                         GenerateOrders()
                     End If
+
                 End SyncLock
             End If
+
             Return orders
         End Function
-        Public ReadOnly Property ContainsListCollection() As Boolean Implements IListSource.ContainsListCollection
+
+        Public ReadOnly Property ContainsListCollection As Boolean Implements IListSource.ContainsListCollection
             Get
                 Return False
             End Get
@@ -78,11 +84,17 @@ Namespace PLinqDataSource
     End Class
 
     Public Class OrderData
-        Public Property OrderId() As Integer
-        Public Property CustomerName() As String
-        Public Property OrderDate() As Date
-        Public Property ProductName() As String
-        Public Property Price() As Decimal
-        Public Property Quantity() As Integer
+
+        Public Property OrderId As Integer
+
+        Public Property CustomerName As String
+
+        Public Property OrderDate As Date
+
+        Public Property ProductName As String
+
+        Public Property Price As Decimal
+
+        Public Property Quantity As Integer
     End Class
 End Namespace
